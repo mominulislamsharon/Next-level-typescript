964042c50788a29e158fc3791b2acddf24e62859
mominulislamsharon <ilsamikchannel328250@gmail.com>
2024-10-31 02:23:49 +0600

    added basic data , write typescript, install typescript

---
+++ module1/dist/test.js
@@ -0,0 +1,2 @@
+"use strict";
+var test = "Iam for testing";
---
+++ module1/src/1.4.ts
@@ -0,0 +1,30 @@
+// Basic Data Type
+
+// string
+
+let firstName : string = 'mominul'
+//number
+let roll: number = 124
+// boolean
+let isAdmin: boolean = true
+// undefined
+let x : undefined = undefined
+// null
+let y: null = null
+
+
+let d;
+d = 123 
+
+let friends : string[] = ['rakib', 'rakiba'];
+friends.push()
+
+let eligableRollList : number[] =[1,3,4,]
+
+// tuple --> array --> order --> type of values
+
+let coordinates: [number,number] = [2,4];
+
+let ageName : [number, string, boolean] = [35, 'mr.mominul', true]
+
+ageName[0] = 35
