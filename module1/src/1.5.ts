66af1a2bd8584756ab9c771e5c8cc27b6c27ffb4
mominulislamsharon <ilsamikchannel328250@gmail.com>
2024-10-31 15:52:46 +0600

    object optional and literal tpes

---
+++ module1/src/1.5.ts
@@ -0,0 +1,12 @@
+// Reference Type ---> Object
+
+const user : {
+  companyName : 'Momin company' // type --> literal types
+  firstName: string;
+  middleName?: string; // optional type
+  lastName: string;
+} = {
+  companyName: 'Momin company',
+  firstName: 'Mominul Islam',
+  lastName:'Sharon'
+}
